067bfd4e10b585a246841df7fac67596a64c8f62
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-02-14 21:53:10 +0200

    added spinner and textfield

--- Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
+++ Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.geeks_love_calculator_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner dropdown_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dropdown_list = findViewById(R.id.programming_languages_list);
     }
+
 }
