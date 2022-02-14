dad32ffa1300a5d13797f83db1fc1b79d6f50a5f
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-02-14 23:08:45 +0200

    added dropdown list array

--- Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
+++ Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.geeks_love_calculator_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dropdown_list = findViewById(R.id.programming_languages_list);
+        String[] text_languages = getResources().getStringArray(R.array.programming_languages);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, text_languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown_list.setAdapter(adapter);
     }
 
 }
