cf680923bb6621a73dfbb6126ef68c09b835f0e1
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-02-15 16:17:25 +0200

    updating calculate_love function in java class

--- Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
+++ Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
@@ -14,19 +14,21 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private static Spinner dropdown_list;
+    String[] text_languages;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dropdown_list = findViewById(R.id.programming_languages_list);
-        String[] text_languages = getResources().getStringArray(R.array.programming_languages);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, text_languages);
+        text_languages = getResources().getStringArray(R.array.programming_languages);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, text_languages);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown_list.setAdapter(adapter);
     }
 
     public void calculate_love(View view){
-        String language = dropdown_list.getContext().toString();
+        String language = dropdown_list.getSelectedItem().toString();
         ImageView image_language = (ImageView) findViewById(R.id.language_logo);
         TextView result = (TextView) findViewById(R.id.result);
         Random random = new Random();
@@ -42,14 +44,17 @@ public class MainActivity extends AppCompatActivity {
         if(language.equals("R")){
             image_language.setImageResource(R.drawable.r);
         }
-        if(language.equals("php")){
+        if(language.equals("PHP")){
             image_language.setImageResource(R.drawable.php);
         }
-        if(language.equals("python")){
+        if(language.equals("Python")){
             image_language.setImageResource(R.drawable.pyhton);
         }
-        if(language.equals("javascript")){
+        if(language.equals("Javascript")){
             image_language.setImageResource(R.drawable.js);
+        }
+        if(language.equals("Java")){
+            image_language.setImageResource(R.drawable.java);
         }
         image_language.setVisibility(View.VISIBLE);
         result.setText("Love Percentage: "+ random.nextInt(100) + " %");
