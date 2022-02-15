8303a9120ec7e505722e64c672d3233d27311040
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-02-15 11:16:22 +0200

    added table layout

--- Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
+++ Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.geeks_love_calculator_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, text_languages);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown_list.setAdapter(adapter);
+    }
+
+    public static void calculate_love(View view){
+
     }
 
 }
