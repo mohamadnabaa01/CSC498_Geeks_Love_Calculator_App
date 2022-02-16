66186be2469fd78bc04b21ef42c08b6cadfc03bd
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2022-02-17 01:35:29 +0200

    debugging some of the code errors, and updating the design xml

--- Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
+++ Geeks_Love_Calculator_App/app/src/main/java/com/example/geeks_love_calculator_app/MainActivity.java
@@ -37,97 +37,106 @@ public class MainActivity extends AppCompatActivity {
         TextView result = (TextView) findViewById(R.id.result);
         Random random = new Random();
         int love_nb = random.nextInt(100);
+        image_language.animate().setDuration(5000).rotation(720).scaleY(1).scaleX(2).alpha(1);
+        image_language.setVisibility(View.VISIBLE);
         if(language.equals("C")){
             image_language.setImageResource(R.drawable.c);
-            TextView language_name = findViewById(R.id.language_c);
-            language_name.setText("C");
-            TextView love_percentage = findViewById(R.id.love_percentage_c);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_c);
+            TextView language_name = (TextView) findViewById(R.id.language_c);
+            String lang = "C";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_c);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_c);
             logo.setImageResource(R.drawable.c);
-            TableRow row = findViewById(R.id.row_c);
+            TableRow row = (TableRow) findViewById(R.id.row_c);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("C++")){
             image_language.setImageResource(R.drawable.cplusplus);
-            TextView language_name = findViewById(R.id.language_cplusplus);
-            language_name.setText("C++");
-            TextView love_percentage = findViewById(R.id.love_percentage_cplusplus);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_cplusplus);
+            TextView language_name = (TextView) findViewById(R.id.language_cplusplus);
+            String lang = "C++";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_cplusplus);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_cplusplus);
             logo.setImageResource(R.drawable.cplusplus);
-            TableRow row = findViewById(R.id.row_cplusplus);
+            TableRow row = (TableRow) findViewById(R.id.row_cplusplus);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("C#")){
             image_language.setImageResource(R.drawable.csharp);
-            TextView language_name = findViewById(R.id.language_csharp);
-            language_name.setText("C#");
-            TextView love_percentage = findViewById(R.id.love_percentage_csharp);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_csharp);
+            TextView language_name = (TextView) findViewById(R.id.language_csharp);
+            String lang = "C#";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_csharp);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_csharp);
             logo.setImageResource(R.drawable.csharp);
-            TableRow row = findViewById(R.id.row_csharp);
+            TableRow row = (TableRow) findViewById(R.id.row_csharp);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("R")){
             image_language.setImageResource(R.drawable.r);
-            TextView language_name = findViewById(R.id.language_r);
-            language_name.setText("R");
-            TextView love_percentage = findViewById(R.id.love_percentage_r);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_r);
+            TextView language_name = (TextView) findViewById(R.id.language_r);
+            String lang = "R";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_r);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_r);
             logo.setImageResource(R.drawable.r);
-            TableRow row = findViewById(R.id.row_r);
+            TableRow row = (TableRow) findViewById(R.id.row_r);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("PHP")){
             image_language.setImageResource(R.drawable.php);
-            TextView language_name = findViewById(R.id.language_php);
-            language_name.setText("PHP");
-            TextView love_percentage = findViewById(R.id.love_percentage_php);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_php);
+            TextView language_name = (TextView) findViewById(R.id.language_php);
+            String lang = "PHP";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_php);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_php);
             logo.setImageResource(R.drawable.php);
-            TableRow row = findViewById(R.id.row_php);
+            TableRow row = (TableRow) findViewById(R.id.row_php);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("Python")){
             image_language.setImageResource(R.drawable.pyhton);
-            TextView language_name = findViewById(R.id.language_python);
-            language_name.setText("Python");
-            TextView love_percentage = findViewById(R.id.love_percentage_python);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_python);
+            TextView language_name = (TextView) findViewById(R.id.language_python);
+            String lang = "Python";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_python);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_python);
             logo.setImageResource(R.drawable.pyhton);
-            TableRow row = findViewById(R.id.row_python);
+            TableRow row = (TableRow) findViewById(R.id.row_python);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("Javascript")){
             image_language.setImageResource(R.drawable.js);
-            TextView language_name = findViewById(R.id.language_js);
-            language_name.setText("Javascript");
-            TextView love_percentage = findViewById(R.id.love_percentage_js);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_js);
+            TextView language_name = (TextView) findViewById(R.id.language_js);
+            String lang = "Javascript";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_js);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_js);
             logo.setImageResource(R.drawable.js);
-            TableRow row = findViewById(R.id.row_js);
+            TableRow row = (TableRow) findViewById(R.id.row_js);
             row.setVisibility(View.VISIBLE);
         }
         if(language.equals("Java")) {
             image_language.setImageResource(R.drawable.java);
-            TextView language_name = findViewById(R.id.language_java);
-            language_name.setText("Java");
-            TextView love_percentage = findViewById(R.id.love_percentage_java);
-            love_percentage.setText(love_nb);
-            ImageView logo = findViewById(R.id.logo_java);
+            TextView language_name = (TextView) findViewById(R.id.language_java);
+            String lang = "Java";
+            language_name.setText(lang);
+            TextView love_percentage = (TextView) findViewById(R.id.love_percentage_java);
+            love_percentage.setText(String.valueOf(love_nb));
+            ImageView logo = (ImageView) findViewById(R.id.logo_java);
             logo.setImageResource(R.drawable.java);
-            TableRow row = findViewById(R.id.row_java);
+            TableRow row = (TableRow) findViewById(R.id.row_java);
             row.setVisibility(View.VISIBLE);
         }
-        image_language.animate().setDuration(5000).rotation(360).scaleX(320).translationYBy(200).alpha(1);
-        image_language.setVisibility(View.VISIBLE);
-        result.setText("Love Percentage: "+ love_nb + " %");
+        String love_text = "Love Percentage: "+ love_nb + " %";
+        result.setText(love_text);
         result.setVisibility(View.VISIBLE);
     }
 
